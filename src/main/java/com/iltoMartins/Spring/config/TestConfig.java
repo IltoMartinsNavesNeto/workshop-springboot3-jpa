4618bbc5ee9cd7060b7608be38f11403b4f807d3
Lealdade1Lugar <iltoneto400@gmail.com>
2024-09-21 13:15:16 -0300

    payment, associação um pra um

--- src/main/java/com/iltoMartins/Spring/config/TestConfig.java
+++ src/main/java/com/iltoMartins/Spring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.iltoMartins.Spring.entities.Category;
 import com.iltoMartins.Spring.entities.Order;
 import com.iltoMartins.Spring.entities.OrderItem;
+import com.iltoMartins.Spring.entities.Payment;
 import com.iltoMartins.Spring.entities.Product;
 import com.iltoMartins.Spring.entities.User;
 import com.iltoMartins.Spring.entities.enums.OrderStatus;
@@ -80,5 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 }
