6518b30cae0105914c9f8c295179fe3ecad98cd0
Lealdade1Lugar <iltoneto400@gmail.com>
2024-09-27 23:25:49 -0300

    Exception handling

--- src/main/java/com/iltoMartins/Spring/services/UserService.java
+++ src/main/java/com/iltoMartins/Spring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.iltoMartins.Spring.entities.User;
 import com.iltoMartins.Spring.repositores.UserRepository;
+import com.iltoMartins.Spring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
